4b85e9cd39451a2ff941f818135ef92573f1daca
Ethelyn Interdictor <einterdi@ge-m5.kzn.21-school.ru>
2021-10-08 16:29:39 +0300

    1

---
+++ ft_strlen.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: einterdi <einterdi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 12:04:04 by einterdi          #+#    #+#             */
+/*   Updated: 2021/10/08 15:36:01 by einterdi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+size_t  ft_strlen(char *str)
+{
+    size_t i;
+
+    i = 0;
+    while (*str != '\0')
+    {
+        str++;
+        i++;
+    }
+    return i;
+}
+
+int main()
+{
+    char a[] = "Denis";
+    printf("%zu\n", ft_strlen(a));
+
+}
