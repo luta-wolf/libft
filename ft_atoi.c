a128c30127d8af1718aab3e54982313b8d3d1451
Ethelyn Interdictor <einterdi@ge-l6.kzn.21-school.ru>
2021-10-13 01:36:48 +0300

    8

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,36 @@
 /*   By: einterdi <einterdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 22:21:48 by einterdi          #+#    #+#             */
-/*   Updated: 2021/10/13 00:23:24 by einterdi         ###   ########.fr       */
+/*   Updated: 2021/10/13 01:35:32 by einterdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int i;
-    int n;
-    int flag;
+	int			i;
+	long int	n;
+	int			flag;
 
-    i = 0;
-    n = 0;
-    flag = 1;
-    while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' 
-        || str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
-        i++;
-    if (str[i] == '-')
-        flag = -1;
-    if (str[i] == '-' || str[i] == '+')
-        i++;
-    while (ft_isdigit(str[i]))
-        {
-            n = n * 10 + str[i] - '0';
-            i++;
-        }           
-    return (n * flag);
+	i = 0;
+	n = 0;
+	flag = 1;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+		flag = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (ft_isdigit(str[i]))
+	{
+		if (flag == 1 && n > 2147483647)
+			return (-1);
+		if (flag == -1 && n < -2147483648)
+			return (0);
+		n = n * 10 + str[i] - '0';
+		i++;
+	}
+	return (n * flag);
 }
